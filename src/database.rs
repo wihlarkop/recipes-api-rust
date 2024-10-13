a1ee64f6740a8a40643338a49d17fb2e830753b8
wihlarkop <wihlarko26@gmail.com>
2024-10-14 03:37:50 +0700

    feat: - modify database init

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,5 @@
 use sqlx::{Error, PgPool, Pool, Postgres};
 
-pub async fn database_init() -> Result<Pool<Postgres>, Error> {
-    PgPool::connect("postgres://postgres:admin@localhost/recipes").await
+pub async fn database_init(database_url: String) -> Result<Pool<Postgres>, Error> {
+    PgPool::connect(&database_url).await
 }
